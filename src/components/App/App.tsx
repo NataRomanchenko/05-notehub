92a46a7c2d0a8a67ac92fc468df4e28a659d808f
Nataliia_Romanchenko <rnataliia36gmail.com>
2026-04-22 12:23:13 +0200

    fix add

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,14 +6,14 @@ import {
 } from "@tanstack/react-query";
 import { useDebouncedCallback } from "use-debounce";
 
-import { fetchNotes, deleteNote } from "./services/noteService";
+import { fetchNotes, deleteNote } from "../../services/noteService";
 
-import NoteList from "./components/NoteList/NoteList";
-import SearchBox from "./components/SearchBox/SearchBox";
-import Pagination from "./components/Pagination/Pagination";
-import Modal from "./components/Modal/Modal";
-import NoteForm from "./components/NoteForm/NoteForm";
-import { createNote } from "./services/noteService";
+import NoteList from "../NoteList/NoteList";
+import SearchBox from "../SearchBox/SearchBox";
+import Pagination from "../Pagination/Pagination";
+import Modal from "../Modal/Modal";
+import NoteForm from "../NoteForm/NoteForm";
+import { createNote } from "../../services/noteService";
 
 export default function App() {
   const queryClient = useQueryClient();
